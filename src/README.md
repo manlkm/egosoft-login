42a42a071223945c67e93391f871000a7be5dbb1
manlkm <novemliu@gmail.com>
2023-01-14 22:14:18 +0800

    enhance product list

---
+++ src/README.md
@@ -0,0 +1,7 @@
+The following packages are required for Component "ProductList":
+"bootstrap-4-react"
+"react-bootstrap-table-next"
+"react-bootstrap-table2-paginator"
+
+react-bootstrap-table-next docs webpage:
+https://react-bootstrap-table.github.io/react-bootstrap-table2/storybook/index.html?selectedKind=Basic%20Table&selectedStory=basic%20table&full=0&addons=1&stories=1&panelRight=0&addonPanel=storybook%2Factions%2Factions-panel
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,17 +3,71 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import '../../node_modules/bootstrap-4-required/src/css/bootstrap.css'
 import '../../node_modules/react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import paginationFactory from 'react-bootstrap-table2-paginator';
+import Button from 'react-bootstrap/Button';
 
 const columns = [{
     dataField: 'name',
     text: '貨品名稱'
 }, {
-    dataField: 'price.value',
-    text: 'Title'
+    dataField: 'summary',
+    text: '規格',
+    formatter: productSummaryFormatter
+},
+{
+    dataField: 'categories[0].name',
+    text: '種類' 
+},
+{
+    dataField: 'df1',
+    isDummyField: true,
+    text: '供應商',
+    formatter: dummySupplierFormatter
 }, {
     dataField: 'price.value',
-    text: 'Completed'
-}];
+    text: '價錢'
+},
+{
+    dataField: 'df2',
+    isDummyField: true,
+    text: '動作',
+    formatter: productActionButtonFormatter
+}
+];
+
+function productActionButtonFormatter(cell, row) {
+    return (
+        <Button variant="success">修改</Button>
+    )
+}
+
+function productSummaryFormatter(cell, row, rowIndex) {
+    let trimmed = '';
+    if (cell != null && cell.length > 0) {
+        trimmed = cell.substring(0, 10).replace(/<[^>]*(>|$)|&nbsp;|&zwnj;|&raquo;|&laquo;|&gt;/g, "");;
+    }
+    return (
+        trimmed
+    );
+}
+
+function dummySupplierFormatter(cell, row) {
+    let supplier = '供應商一'
+    if (row.price.value != null) {
+        if (row.price.value > 200 && row.price.value <= 300) {
+            supplier = '供應商二';
+        }
+        else if (row.price.value > 300 && row.price.value <= 500) {
+            supplier = '供應商三';
+        }
+        else {
+            supplier = '供應商一';
+        }
+    }
+
+    return (
+        supplier
+    );
+}
 
 export default class ProductList extends Component {
 
@@ -37,7 +91,7 @@ export default class ProductList extends Component {
         await fetch(`${url}`, obj)
             .then((response) => response.json())
             .then((responseJson) => {
-                console.warn(responseJson);
+                //console.warn(responseJson);
                 this.setState({ products: responseJson })
             })
             .catch((error) => {
@@ -47,19 +101,18 @@ export default class ProductList extends Component {
 
     componentDidMount() {
         this.getProducts();
-        console.log('products', this.state.products);
     }
 
     render() {
         return (
             <BootstrapTable
-                keyField="id"
+                keyField="name"
                 data={this.state.products}
                 columns={columns}
                 striped
                 hover
                 condensed
-                pagination={ paginationFactory() }
+                pagination={paginationFactory()}
             />
         )
     }
